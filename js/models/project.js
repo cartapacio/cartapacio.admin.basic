baa550eb74e30013c6d2571817ff738dd137304a
Gabriel Zea <zea@randomlab.net>
2014-05-09 15:31:33 -0500

    defaults for project

---
+++ js/models/project.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var $ = require('jquery'),
+  Backbone = require('backbone')
+
+Backbone.$ = $
+
+module.exports = Backbone.Model.extend({
+  schema: {
+    title: 'Text',
+    description: 'TextArea'
+  },
+
+  defaults:{
+    doctype: 'project',
+    title: 'title of the project',
+    description: 'information about this',
+    date: 'the format you prefer',
+    links:[
+      {
+        name: 'where?',
+        url: 'url'
+      }
+    ],
+    images:[
+      {
+        title: 'title or short description',
+        file: 'local uri'
+      }
+    ],
+    videos: [
+      {
+        code: 'embed code given by the provider'
+      }
+    ]
+  }
+});
+
