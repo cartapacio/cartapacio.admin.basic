48eef438b61ba60510b4d2918a986bade7515d3e
Gabriel Zea <zea@randomlab.net>
2014-05-23 14:04:54 -0500

    rich text editor on news form

--- js/views/news/NewNewsView.js
+++ js/views/news/NewNewsView.js
@@ -4,6 +4,7 @@ var $ = require('jquery'),
   Backbone = require('backbone'),
   FormSerializer = require('form-serializer'),
   serializer = new FormSerializer($),
+  grande = require('grande-module'),
   model = require('../../models/NewsModel'),
   template = require('../../../templates/NewNews.hbs')
 
@@ -24,7 +25,10 @@ module.exports = Backbone.View.extend({
   render: function(){
     this.template = template(this.model.attributes)
     // Dynamically updates the UI with the view's template
-    this.$el.html(this.template);
+    this.$el.html(this.template)
+
+    grande.bind($(this.template).find('#body')[0])
+
     return this
   },
 
@@ -35,6 +39,7 @@ module.exports = Backbone.View.extend({
     var doc = serializer.addPairs(info).serialize()
 
     doc.date = new Date()
+    doc.body = $('#body')[0].innerHTML
 
     this.model.save(doc,{
       success: function(model){
